b92d524f17b4d9dd0fa99451d5394e42659ff05f
aluno-uffs <aluno-uffs@lab-comp.cco.uffs.edu.br>
2018-08-15 20:54:10 -0300

    game quase pronto

--- poker.py
+++ poker.py
@@ -1,4 +1,5 @@
 # Imports:
+import bottle
 import random
 
 # Definition of Class CARD:
@@ -42,16 +43,17 @@ class Deck(object):
     def draw(self):
         return self.cards.pop()
 
-# Definition of Class PLAYER:
+# Definition of Class PLAYER:python3-bottle
 class Player(object):
     # Inicialization Method:
     def __init__(self, name):
         self.name = name
         self.hand = []
+        self.points = 0
         return
     # Show Hand Method:
     def showHand(self):
-        print("{}\'s Hand:".format(self.name))
+        print("{}\'s Hand:".format(self.name), end="")
         for card in self.hand:
             card.show()
         print()
@@ -76,22 +78,24 @@ class Table(object):
         self.name = name
         self.deck = Deck()
         self.discard = []
-        self.flop = []
-        self.turn = []
-        self.river = []
+        self.cards = []
         self.players = []
-        return
-    # Build Method:
-    def build(self):
-        self.deck.shuffle()
         return
     # Start Game Method:
     def start(self):
+        while self.discard:
+            self.deck.cards.append(self.discard.pop())
+        self.deck.shuffle()
+        for player in self.players:
+            player.points = 0
         for n in range(2):
             for player in self.players:
                 player.draw(self.deck)
         for n in range(3):
-            self.flop.append(self.deck.cards.pop())
+            self.cards.append(self.deck.cards.pop())
+        for n in range(2):
+            self.discard.append(self.deck.cards.pop())
+            self.cards.append(self.deck.cards.pop())
         return
     # Show Deck Method:
     def showDeck(self):
@@ -102,21 +106,9 @@ class Table(object):
     # Show Table Cards Method:
     def showCards(self):
         print("===== {} Table Cards =====".format(self.name))
-        if self.flop:
-            for card in self.flop:
-                card.show()
-        else:
-            print("[ #### ]"*3, end='')
-        if self.turn:
-            for card in self.turn:
-                card.show()
-        else:
-            print("[ #### ]", end='')
-        if self.river:
-            for card in self.river:
-                card.show()
-        else:
-            print("[ #### ]")
+        for card in self.cards:
+            card.show()
+        print()
     # Show Players Method:
     def showPlayers(self):
         print("===== {} Players =====".format(self.name))
@@ -125,10 +117,23 @@ class Table(object):
         print()
         return
 
-# MAIN of the Program:
-table = Table("Table 01")
-table.build()
+# Definition of Class Dealer:
+class Dealer(object):
+    # Inicialization Method:
+    def __init__(self):
+        self.weight = {"A":13, "K":12, "Q":11, "J":10, "10":9, "9":8, "8":7, "7":6, "6":5, "5":4, "4":3, "3":2, "2":1}
+        self.multiplier = {"♣":2, "❤":1.5, "♠":1, "♦":0.5}
+        return
+    # Player Hand Evaluation:
+    def evaluateHand(self, table, player):
+        playerPoints = 0
+        for card in player.hand:
+            playerPoints += self.weight[str(card.value)]*self.multiplier[str(card.suit)]
+        return playerPoints
 
+# MAIN of the Program:
+dealer = Dealer()
+table = Table("Table 01")
 players = []
 for player in range(1,10):
     players.append(Player("Player " + str(player)))
@@ -138,3 +143,4 @@ for player in players:
 table.start()
 table.showCards()
 table.showPlayers()
+print(dealer.evaluateHand(table, players[0]))
